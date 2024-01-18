e2865eb31ae6f73f4a7d0e35dda5522068b500a0
Dmitry Grigorovich <dmitry.grigorovich7@gmail.com>
2024-01-18 15:17:07 +0200

    Update Sink

--- include/logix/sinks/Sink.h
+++ include/logix/sinks/Sink.h
@@ -15,28 +15,28 @@ namespace logix
 	class Sink
 	{
 	public:
-        Sink() = default;
-        virtual ~Sink() = default;
+        	Sink() = default;
+        	virtual ~Sink() = default;
 
-        Sink(const Sink&) = delete;
-        Sink(Sink&&) = delete;
+        	Sink(const Sink&) = delete;
+        	Sink(Sink&&) = delete;
 
-        Sink& operator=(const Sink&) = delete;
-        Sink& operator=(Sink&&) = delete;
+        	Sink& operator=(const Sink&) = delete;
+        	Sink& operator=(Sink&&) = delete;
 
-        virtual void log(const LogMessage& msg) final;
-        virtual void flush() final;
+        	virtual void log(const LogMessage& msg) final;
+        	virtual void flush() final;
 
-        LogLevel level() const;
-        void setLevel(LogLevel logLevel);
-        bool shouldLog(LogLevel msgLogLevel) const;
+        	LogLevel level() const;
+        	void setLevel(LogLevel logLevel);
+        	bool shouldLog(LogLevel msgLogLevel) const;
 
-    protected:
-        virtual void logImpl(const LogMessage& msg) = 0;
-        virtual void flushImpl() = 0;
+    	protected:
+        	virtual void logImpl(const LogMessage& msg) = 0;
+        	virtual void flushImpl() = 0;
 
-        std::mutex m_mutex;
-        std::atomic_uchar m_logLevel { static_cast<std::underlying_type<LogLevel>::type>(LogLevel::Trace) };
+        	std::mutex m_mutex;
+        	std::atomic_uchar m_logLevel { static_cast<std::underlying_type<LogLevel>::type>(LogLevel::Trace) };
 	};
 
 } // namespace logix
